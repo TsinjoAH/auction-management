163e6b0d40a91891ea14295d597beb91283dc321
TsinjoAH <52tsinjo@gmail.com>
2023-01-14 00:07:47 +0300

    refactoring

--- auction-back-end/src/main/java/com/management/auction/repos/userSignUpRepo.java
+++
@@ -1,7 +0,0 @@
-package com.management.auction.repos;
-
-import com.management.auction.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface userSignUpRepo extends JpaRepository<User,Long> {
-}
