5437a4b84c709d294cb31e48470210981edcdf27
rova30 <rovarobel30@gmail.com>
2023-01-15 22:48:48 +0300

    Returning AuctionView

--- auction-back-end/src/main/java/com/management/auction/services/AuctionService.java
+++ auction-back-end/src/main/java/com/management/auction/services/AuctionService.java
@@ -3,10 +3,12 @@ package com.management.auction.services;
 import com.management.auction.models.auction.Auction;
 import com.management.auction.models.User;
 import com.management.auction.models.auction.AuctionPic;
+import com.management.auction.models.auction.AuctionView;
 import com.management.auction.repos.auction.AuctionPicRepo;
 import com.management.auction.models.auction.AuctionReceiver;
 import com.management.auction.repos.auction.AuctionRepo;
 import com.management.auction.repos.BidRepo;
+import com.management.auction.repos.auction.AuctionViewRepo;
 import custom.springutils.exception.CustomException;
 import custom.springutils.service.CrudServiceWithFK;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,15 +17,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AuctionService extends CrudServiceWithFK<Auction, User, AuctionRepo> {
+    private final AuctionViewRepo auctionViewRepo;
     private final AuctionPicRepo auctionPicRepository;
     @Autowired
     private BidRepo bidRepo;
     public AuctionService(AuctionRepo repo,
-                          AuctionPicRepo auctionPicRepository) {
+                          AuctionViewRepo auctionViewRepo, AuctionPicRepo auctionPicRepository) {
         super(repo);
+        this.auctionViewRepo = auctionViewRepo;
         this.auctionPicRepository = auctionPicRepository;
     }
 
@@ -31,6 +36,11 @@ public class AuctionService extends CrudServiceWithFK<Auction, User, AuctionRepo
     public List<Auction> findForFK(User user) {
         return this.repo.findByUserId(user.getId());
     }
+
+    public List<AuctionView> findForFKView(User user) {
+        return this.auctionViewRepo.findByUserId(user.getId());
+    }
+
 
     @Override
     public Auction findById(Long id) {
@@ -41,6 +51,12 @@ public class AuctionService extends CrudServiceWithFK<Auction, User, AuctionRepo
         auction.setBids(bidRepo.findByAuctionId(id));
         return auction;
     }
+
+    public AuctionView findByIdView(Long id) {
+        Optional<AuctionView> auctionViewOptional = auctionViewRepo.findById(id);
+        return auctionViewOptional.get();
+    }
+
 
     @Transactional
     public Auction create(AuctionReceiver auctionReceiver) throws CustomException, IOException {
