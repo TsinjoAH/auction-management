5ff370ecdc564c35efd712a4a8c57d1f1d9d759b
rova30 <rovarobel30@gmail.com>
2023-01-15 22:45:00 +0300

    Update AuctionView.java

--- auction-back-end/src/main/java/com/management/auction/models/auction/AuctionView.java
+++ auction-back-end/src/main/java/com/management/auction/models/auction/AuctionView.java
@@ -1,6 +1,5 @@
 package com.management.auction.models.auction;
 
-import com.management.auction.models.Bid;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -10,15 +9,13 @@ import jakarta.persistence.Table;
 @Entity
 public class AuctionView extends AuctionBase{
     @Column
-    private String status;
+    private Integer status;
 
-
-    public String getStatus() {
+    public Integer getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(Integer status) {
         this.status = status;
     }
-
 }
