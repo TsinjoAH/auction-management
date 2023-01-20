e56360e50f33c5c31efd6e267b317eed1627aa9e
Lars Ratovo <lars.ratovo@gmail.com>
2023-01-20 19:07:33 +0300

    depl

--- auction-back-end/src/main/java/com/management/auction/Main.java
+++
@@ -1,9 +0,0 @@
-package com.management.auction;
-
-import org.apache.commons.codec.digest.DigestUtils;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(DigestUtils.sha1Hex("lahatra".getBytes()));
-    }
-}
